eb0b07d786d13e14f33c749a10959fc75783397e
Diegocuello <127902683+Diegocuello@users.noreply.github.com>
2024-10-09 17:49:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,33 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('¡Mensaje enviado!');
 });
+// Mostrar y ocultar secciones
+document.getElementById('botonSemanales').addEventListener('click', function() {
+    mostrarSeccion('semanales');
+});
+document.getElementById('botonQuincenales').addEventListener('click', function() {
+    mostrarSeccion('quincenales');
+});
+document.getElementById('botonPedidos').addEventListener('click', function() {
+    mostrarSeccion('pedidos');
+});
+
+function mostrarSeccion(seccionId) {
+    document.getElementById('semanales').style.display = 'none';
+    document.getElementById('quincenales').style.display = 'none';
+    document.getElementById('pedidos').style.display = 'none';
+    document.getElementById(seccionId).style.display = 'block';
+}
+
+// Guardar pedidos
+document.getElementById('guardarPedido').addEventListener('click', function() {
+    var pedidoTexto = document.getElementById('pedidoTexto').value;
+    if (pedidoTexto) {
+        var listaPedidos = document.getElementById('listaPedidos');
+        var nuevoPedido = document.createElement('div');
+        nuevoPedido.textContent = pedidoTexto;
+        listaPedidos.appendChild(nuevoPedido);
+        document.getElementById('pedidoTexto').value = '';
+    }
+});
 
